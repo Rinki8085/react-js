22098903f53a10c6256a00f491b9e8995384c166
Rinki8085 <krinki2604@gmail.com>
2021-10-18 22:00:54 +0530

    ReactJS

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h2>Header</h2>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
