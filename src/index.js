b5c93e8eea974cfb06ff45c0b9f7d19dc84b28e9
Rinki8085 <krinki2604@gmail.com>
2021-10-20 15:15:10 +0530

    ReactJS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-
-const App = () => {
-    return(
-        <div>
-            <Header>
-            <h1>Hii From Rinki</h1>
-            <h2>Test</h2>
-        </div>
-    )
-}
-
-ReactDOM.render(<App/>,document.getElementById('root'))
